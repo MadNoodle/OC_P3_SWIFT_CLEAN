e5e61a4ee4862b8d97428f428d378223e5439745
MadNoodle <mjanneau@gmail.com>
2017-10-13 14:50:25 +0200

    ajout de la création de l'équipe joueur// plusieurs bugs notésp

--- PROJET3.swift
+++ PROJET3.swift
@@ -8,19 +8,19 @@ class Character {
     let name : String // Nom du combattant
     var classe : Classe//Profession champion
     var health : Int // Points de vie
-    var damages : Int // Valeurs Degats ou soins pour mages
+    var damages : Int // Valeurs Dégats ou soins pour mages
     
     init(name:String, classe : Classe){
         self.name = name
         self.classe = classe
-        
+        //Caracteristiques des classes
         switch self.classe{
         case .warrior:
                 self.health = 100
                 self.damages = 10
         case .mage:
                 self.health = 50
-                self.damages = 20
+                self.damages = -20 //valeur négative car soins (trouver une manière plus élégante)
         case .colossus:
                 self.health = 150
                 self.damages = 2
@@ -29,7 +29,6 @@ class Character {
                 self.damages = 30
         }
     }
-
 }
 
 enum Classe {
@@ -38,57 +37,81 @@ enum Classe {
 
 // MARK: FUNCTIONS
 
-// Donner un nom à son champion
+// CREER UN CHAMPION
 
-func createHero(){
+func createHero() -> Character{
+    // Donner un nom à son champion
     print ("comment s'appelle votre guerrier?")
     var name = ""
     if let nameChamp = readLine(){
         name = nameChamp
         print("Votre héros s'appelle \(nameChamp)")
     }
-    
-    print ("Quelle est la profession du champion?"
+    // Choisir sa classe
+    print ("Quelle est sa profession ?"
         + "\n1. Guerrier"
         + "\n2. Mage"
         + "\n3. Colosse"
         + "\n4. Nain")
-    
+  
     var classe:Classe!
+    // On assume qu'il a une valeur donc il faut verifier avant l entrée dans le switch
     if let choice = readLine(){
         switch choice {
         case "1":
             classe = .warrior
-            print("vous avez choisit un guerrier")
         case "2":
             classe = .mage
-            print("vous avez choisit un mage")
         case "3":
             classe = .colossus
-            print("vous avez choisit un colosse")
         case "4":
             classe = .dwarf
-            print("vous avez choisit un nain")
         default:
-            print("Choisissez une classe")
+           print("Veuillez choisir une classe")
+/**
+- ToDo: gerer erreur quand on tape autre chose que les choix
+*/
         }
     }
-    
-    let champion1 = Character(name:name,classe:classe)
-    let description = "================================="
-                    + "\n      Nom :    \(champion1.name) "
-                    + "\n      Classe : \(champion1.classe)"
-                    + "\n      Vie :    \(champion1.health) pts"
-                    + "\n      Dmg :    \(champion1.damages) pts"
-                    + "\n================================="
-    
-    print(description)
+    //Création du champion
+    let champion = Character(name:name,classe:classe)
+    return champion
 }
 
+func createPlayerTeam(){
+    while playerTeam.count<4{
+        let hero = createHero()
+        
+/**
+ - ToDo: Ajouter une condition pour ne pas avoir de doublons dans les noms
+ */
+        
+        //ajout du hero généré dans le tableau
+            playerTeam.append(hero)
+        //limite la taille de l'équipe à 4 Héros
+            for i in 0..<playerTeam.count {
+                print(
+                "==========Heros n°\(i+1)=============="
+                    + "\n      Nom :    \(playerTeam[i].name) "
+                    + "\n      Classe : \(playerTeam[i].classe)"
+                    + "\n      Vie :    \(playerTeam[i].health) pts"
+                    + "\n      Dmg :    \(playerTeam[i].damages) pts"
+                    + "\n=================================")
+            }
+            
+        
+        
+        if playerTeam.count == 4 {
+            print("=================Vous etes pret à combrattre================")
+        }
+    }
+}
+// MARK: GAME
 
-// MARK: DIALOGUES
+//tableau qui contiendra l'équipe du jour
+var playerTeam = [Character]()
 
-createHero()
+createPlayerTeam()
 
 
 
