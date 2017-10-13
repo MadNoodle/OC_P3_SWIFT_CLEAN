dda3e98853faabc073214bf5a29c81735f3e1e07
MadNoodle <mjanneau@gmail.com>
2017-10-13 11:49:19 +0200

    ajout de fonction pour nommer un personnage

--- PROJET3.swift
+++ PROJET3.swift
@@ -6,7 +6,7 @@
 //Defintion Objet personnage qui stocke les caracteristiques communes
 class Character {
     let name : String // Nom du combattant
-    let classe : String //Profession champion
+    var classe : String //Profession champion
     var health : Int //pts de vie
     
     init(name:String, classe : String, health : Int){
@@ -17,16 +17,42 @@ class Character {
 
 }
 
+enum Classe {
+    case warrior, mage, colossus, dwarf
+}
+
+// MARK: FUNCTIONS
+
+// Donner un nom à son champion
+func nameChamp() -> Character? {
+    if let nameChamp = readLine(){
+        let champion = Character(name:nameChamp, classe:"", health:100)
+        print("votre personnage s'appelle \(champion.name) et a \(champion.health)pts de vie")
+        return champion
+    }
+    return nil
+    
+}
+
+
 // MARK : DIALOGUES
 
-
-// Choisir le nom de son guerrier
+// ETAPE 1 :Choisir le nom de son guerrier
 print ("comment s'appelle votre guerrier?")
+nameChamp()
 
-if let nameChamp = readLine(){
-    let champion = Character(name:nameChamp, classe:"testeur", health:100)
-    print("votre personnage est un \(champion.classe), il s'appelle \(champion.name) et a \(champion.health)pts de vie")
-}
+// ETAPE 2: Choisir une classe
+print("Veuillez choisir votre champion"
+    + "\n 1. Combattant"
+    + "\n 2. Mage"
+    + "\n 3. Colosse"
+    + "\n 4. Nain")
+
+
+
+
+
+
 
 
 
