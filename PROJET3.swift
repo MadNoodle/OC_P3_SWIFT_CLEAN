8c846448cef5836c4cba79e30a31473094c5258f
MadNoodle <mjanneau@gmail.com>
2017-10-14 19:25:00 +0200

    ajout de la fonction die et amelioration des winning conditions

--- PROJET3.swift
+++ PROJET3.swift
@@ -18,17 +18,19 @@ class Game {
         player2.createPlayerTeam()
         player2.showTeam()
    
-    //Fight loop
         //TURN BY TURN LOGIC
         while player1.playerTeam.count>0 || player2.playerTeam.count>0{
             player1.turn(enemyPlayer:player2)
             player2.turn(enemyPlayer:player1)
         }
     
-       
-    //Winning conditions
-    if player1.playerTeam.count == 0 {print("LE JOUEUR 2 à GAGNER")} else if player2.playerTeam.count == 0 {print("LE JOUEUR 1 à GAGNER")}
-     }
+        //WINNING CONDITIONS
+/**
+Todo:enhance Winning messageCONDITIONS
+*/
+        if player1.playerTeam[0].health == 0 && player1.playerTeam[1].health == 0 && player1.playerTeam[2].health == 0 {print("LE JOUEUR 2 à GAGNER")}
+        else if player2.playerTeam[0].health == 0 && player2.playerTeam[1].health == 0 && player2.playerTeam[2].health == 0 {print("LE JOUEUR 1 à GAGNER")}
+         }
 }
 
 // ///////////////  //
@@ -44,7 +46,11 @@ class Player {
         self.id = id
     }
     
-    //Create a team
+    // //////////////////////////  //
+    // MARK: SELECT YOUR WARRRIORS //
+    // //////////////////////////  //
+    
+    //CREATE TEAM
     func createPlayerTeam(){
         
         print("\u{001B}[0;33mLE JOUEUR \(self.id) VA COMPOSER SON EQUIPE\u{001B}[0;37m")
@@ -68,28 +74,72 @@ class Player {
         }
     }
     
+    // CREATE HERO
+    
+    func createHero() -> Character{
+        // Give a name to a champ
+        print ("comment s'appelle votre guerrier?")
+        var name = ""
+        if let nameChamp = readLine(){
+            name = nameChamp
+            print("Votre héros s'appelle \(nameChamp)")
+        }
+        // Choose character class
+        print ("Quelle est sa profession ?"
+            + "\n1. Guerrier"
+            + "\n2. Mage"
+            + "\n3. Colosse"
+            + "\n4. Nain")
+        
+        var classe:Classe!                  // Poser question Dhia "Pourquoi je dois le forcer"
+        // On assume qu'il a une valeur donc il faut verifier avant l entrée dans le switch
+        if let choice = readLine(){
+            switch choice {
+            case "1":
+                classe = .warrior
+            case "2":
+                classe = .mage
+            case "3":
+                classe = .colossus
+            case "4":
+                classe = .dwarf
+            default:
+                print("ERROR")
+            }
+        }
+        /**
+         -ToDo : Corriger bug si pas 1234
+         */
+        //Hero instanciantion
+        let champion = Character(name:name,classe:classe)
+        return champion
+    }
+
+    
+   
+    //SHOW CHAR DETAILS
+    func showTeamDetail (id:Int) ->String {
+        let detail = "                  Nom: \(playerTeam[id].name) - \(playerTeam[id].icon)    Classe : \(playerTeam[id].classe) -     ❤️  vie:  \(playerTeam[id].health) pts -    ⚔  Dommages: \(playerTeam[id].damages)"
+        return detail
+    }
+    
     // SHOW TEAM STATS
     func showTeam() {
         for i in 0..<playerTeam.count {
             print(
-                "==========Heros n°\(i+1)=============="
-                    + "\n      Nom :    \(playerTeam[i].name) "
-                    + "\n      Classe : \(playerTeam[i].classe)"
-                    + "\n      Vie :    \(playerTeam[i].health) pts"
-                    + "\n      Dmg :    \(playerTeam[i].damages) pts"
-                    + "\n=================================")
+                "====Heros n°\(i+1)========================================================================================"
+                + "\n \(showTeamDetail(id:i))"
+                + "\n=====================================================================================================")
         }
     }
-    //SHOW CHAR DETAILS
-    func showTeamDetail (id:Int) ->String {
-        let detail = "\(playerTeam[id].name) \(playerTeam[id].classe) \(playerTeam[id].health) \(playerTeam[id].damages)"
-        return detail
-    }
+    
+    // //////////////////////  //
+    // MARK: PLAYER TURN CHOICE //
+    // //////////////////////  //
     // Different actions a player can do during a turn
     func turn(enemyPlayer:Player){
         
-        print ("\n\n"
-            +"=====================AU TOUR DU JOUEUR \(self.id) DE JOUER====================="
+        print ("=====================AU TOUR DU JOUEUR \(self.id) DE JOUER====================="
             + "\n Que souhaitez vous faire?"
             + "\n1. 👁 voir l'état de votre équipe"
             + "\n2. ⚔ attaquer")
@@ -101,34 +151,40 @@ class Player {
                 showTeam()
                 turn(enemyPlayer:enemyPlayer)
             case "2":
-                figth(enemyPlayer:enemyPlayer)
-            case "3":
-                print("soigner")
+                fight(enemyPlayer:enemyPlayer)
             default:
                 print("Je ne comprends pas")
             }
         }
     }
     
-    func figth(enemyPlayer:Player){
+    // ///////////////////  //
+    // MARK: FIGHT MECANICS //
+    // //////////////////  //
+    func fight(enemyPlayer:Player){
+        // Array where we store attacker and target ( will be cleared after each turn)
         var fighter = [Character]()
-      
-        
+
+        //Choose your fighter
         print("Choisissez votre attaquant"
             + "\n 1. \(self.showTeamDetail(id:0))"
             + "\n 2. \(self.showTeamDetail(id:1))"
             + "\n 3. \(self.showTeamDetail(id:2))")
-
+/**
+ Todo: Add an check to disable deadd playersAdd
+ */
         if let choice = readLine(){
             var attacker:Character
             switch choice {
             case "1":
                 attacker = self.playerTeam[0]
                 fighter.append(attacker)
+           
             case "2":
                 attacker = self.playerTeam[1]
                 fighter.append(attacker)
             case "3":
+
                 attacker = self.playerTeam[2]
                 fighter.append(attacker)
             default:
@@ -136,7 +192,7 @@ class Player {
             }
             
         }
-        
+        //Choose your victim
         print("Choisissez votre cible"
             + "\n 1. \(enemyPlayer.showTeamDetail(id:0)) "
             + "\n 2. \(enemyPlayer.showTeamDetail(id:1)) `"
@@ -144,20 +200,26 @@ class Player {
         
         if let choice = readLine(){
               var target:Character
+            
+          
+   /**ToDo: handle the case where it s healer and print a different messageand*/
             switch choice {
             case "1":
                 target = enemyPlayer.playerTeam[0]
                 fighter.append(target)
                 attack(attacker:fighter[0], target:fighter[1])
+                fighter.removeAll()
 
             case "2":
                 target = enemyPlayer.playerTeam[1]
                 fighter.append(target)
                 attack(attacker:fighter[0], target:fighter[1])
+                fighter.removeAll()
             case "3":
                 target = enemyPlayer.playerTeam[2]
                 fighter.append(target)
                 attack(attacker:fighter[0], target:fighter[1])
+                fighter.removeAll()
             default:
                 print("Je ne comprends pas")
             }
@@ -165,14 +227,26 @@ class Player {
         }
 
     }
-    
+    // ATTACK => DO DAMAGES
     func attack(attacker:Character, target:Character){
-        target.health -= attacker.damages
-        print("\(attacker.name) fait \(attacker.damages) points de dommage à\(target.name). il lui reste \(target.health) points de vie.")
-         fighter.removeAll()
+        //Deal Damage
+            target.health -= attacker.damages
+        //Check if the character dies
+        if target.health <= 0 {
+            print ("☠️  \(target.name) est mort☠️ ☠️")
+            target.damages = 0
+            target.icon = "☠️"
+        } else {
+            
+            print("\(attacker.name) fait \(attacker.damages) points de dommage à\(target.name). il lui reste \(target.health) points de vie.")
+        }
+        
     }
+    
+   
 
 }
+
 // ///////////////  //
 // MARK: CHARACTERS //
 // ///////////////  //
@@ -183,6 +257,7 @@ class Character {
     var classe : Classe //Fighter's class
     var health : Int // health points
     var damages : Int // damages or heal value
+    var icon : String // class icon
     
     init(name:String, classe : Classe){
         self.name = name
@@ -190,70 +265,31 @@ class Character {
         //class caracteristics
         switch self.classe{
         case .warrior:
-                self.health = 100
+                self.health = 10
                 self.damages = 10
+                self.icon = "⚔"
         case .mage:
                 self.health = 50
                 self.damages = -20
+                self.icon = "✚"
         case .colossus:
                 self.health = 150
                 self.damages = 2
+                self.icon = "⛰"
         case .dwarf:
                 self.health = 25
                 self.damages = 30
+                self.icon = "⚒"
         }
     }
-
+    
+    
 }
 //Enum of all classes
 enum Classe {
     case warrior, mage, colossus, dwarf
 }
 
-// ///////////////  //
-// MARK: FUNCTIONS  //
-// ///////////////  //
-
-// Create a hero
-
-func createHero() -> Character{
-    // Give a name to a champ
-    print ("comment s'appelle votre guerrier?")
-    var name = ""
-    if let nameChamp = readLine(){
-        name = nameChamp
-        print("Votre héros s'appelle \(nameChamp)")
-    }
-    // Choose character class
-    print ("Quelle est sa profession ?"
-        + "\n1. Guerrier"
-        + "\n2. Mage"
-        + "\n3. Colosse"
-        + "\n4. Nain")
-    
-    var classe:Classe!                  // Poser question Dhia "Pourquoi je dois le forcer"
-                                        // On assume qu'il a une valeur donc il faut verifier avant l entrée dans le switch
-    if let choice = readLine(){
-        switch choice {
-        case "1":
-            classe = .warrior
-        case "2":
-            classe = .mage
-        case "3":
-            classe = .colossus
-        case "4":
-            classe = .dwarf
-        default:
-            print("ERROR")
-            }
-        }
-/**
- -ToDo : Corriger bug si pas 1234
- */
-    //Hero instanciantion
-    let champion = Character(name:name,classe:classe)
-    return champion
-    }
 
 
 // ///////////////  //
@@ -261,7 +297,6 @@ func createHero() -> Character{
 // ///////////////  //
 let player1 = Player(id:1)
 let player2 = Player(id:2)
-
 let game = Game(player1:player1,player2:player2)
 game.newGame()
 
